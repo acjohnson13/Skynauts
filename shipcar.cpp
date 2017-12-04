7c1ed63d6d191d40afe3b87a0554ba58fb3bb8d0
zaphillips14 <zaphillips14@my.trine.edu>
2017-12-04 13:46:54 -0500

    Prevention from players moving off the screen
    Keeps the players from moving off the screen horizontally.

--- shipcar.cpp
+++ shipcar.cpp
@@ -5,15 +5,27 @@ void shipcar::keyPressEvent(QKeyEvent *event)
 {
     if (event->key() == Qt::Key_A){
         setPos(x()-10,y());
+        if(pos().x() < 0){
+            setPos(0, y());
+        }
     }
     else if(event->key() == Qt::Key_D){
         setPos(x()+10, y());
+        if(pos().x() > 980){
+            setPos(980, y());
+        }
     }
     else if(event->key() == Qt::Key_Left){
         setPos(x()-10,y());
+        if(pos().x() < 0){
+            setPos(0, y());
+        }
     }
     else if(event->key() == Qt::Key_Right){
         setPos(x()+10,y());
+        if(pos().x() > 980){
+            setPos(980, y());
+        }
     }
 }
 
